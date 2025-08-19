5b2208824be4736791bae857db326724618805af
Douglas Ribeiro Costa <douglasrc2008@gmail.com>
2025-08-18 21:38:46 -0300

    Variaveis Globais em Assemblt AMD64

---
+++ Segurança-de-Sistemas/maispilha.s
@@ -0,0 +1,28 @@
+.section .data
+    A: .quad 0
+    B: .quad 0
+.section .text
+globl _start
+
+soma:
+    pushq %rbp
+    movq %rsp, %rbp
+    subq $16, %rsp  #Alocamos as duas Variaveis globais (A e B) na pilha. Subtriando por 16 = 8.2
+    movq A, %rax
+    movq %rax, -8(%rbp)
+    movq B, %rbx
+    movq %rbx, -16(%rbp)
+    movq -8(%rbp), %rax
+    movq -16(%rbp), %rbx
+    addq %rbx, %rax
+    popq %rbp
+    ret
+
+_start: 
+    movq $4, A
+    movq $5, B
+
+    call soma
+    movq %rax, %rdi
+    movq $60, %rax
+    syscall
